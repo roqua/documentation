2569c9064d6347826df7afa58f9932e327f66938
Marten Veldthuis <marten@veldthuis.com>
2025-01-20 14:32:57 +0100

    Move roqua changelog, add real blog

---
+++ blog/20210915-self-initiables.md
@@ -0,0 +1,15 @@
+### Eigen initiatief vragenlijsten
+
+Er is nu een mogelijkheid om respondenten op eigen initiatief vragenlijsten in te laten vullen.
+
+Als dit voor de meting van dit protocol door een coördinator is aangezet, komt er een extra knop beschikbaar bij het klaarzetten van de lijst(en): "Eigen initiatief toestaan".
+
+![Aanzetten](https://roqua-announcements.s3.eu-west-1.amazonaws.com/self_report_enable.png)
+
+Als je hier op klikt krijgt de respondent voortaan als ze de normale invullink volgen naast hun klaarstaande lijsten ook altijd de mogelijkheid deze lijst(en) in te vullen, zo vaak als ze maar willen.
+
+![Aanzetten](https://roqua-announcements.s3.eu-west-1.amazonaws.com/self_report_fill_out.png)
+
+Bovenaan de overzichtspagina kan het recht om op eigen initiatief te mogen invullen weer ingetrokken worden.
+
+![Aanzetten](https://roqua-announcements.s3.eu-west-1.amazonaws.com/self_report_timeline.png)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,59 +1,59 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'RoQua Documentation',
-  tagline: '',
-  favicon: 'img/favicon.ico',
+  title: "RoQua Documentation",
+  tagline: "",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://docs.roqua.net',
+  url: "https://docs.roqua.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'roqua', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: "roqua", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'nl',
-    locales: ['nl', 'en'],
+    defaultLocale: "nl",
+    locales: ["nl", "en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://gitlab.roqua.nl/roqua/pages/documentation/-/tree/master/',
+            "https://gitlab.roqua.nl/roqua/pages/documentation/-/tree/master/",
         },
         blog: {
           blogSidebarCount: 50,
-          blogSidebarTitle: "Releases",
+          blogSidebarTitle: "Blog",
           showReadingTime: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -61,56 +61,92 @@ const config = {
 
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'technical',
-        path: 'technical',
-        routeBasePath: 'technical',
-        sidebarPath: require.resolve('./sidebars.js'),
+        id: "technical",
+        path: "technical",
+        routeBasePath: "technical",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+
+    [
+      "@docusaurus/plugin-content-blog",
+      {
+        /**
+         * Required for any multi-instance plugin
+         */
+        id: "changelog-roqua-production",
+        /**
+         * URL route for the blog section of your site.
+         * *DO NOT* include a trailing slash.
+         */
+        routeBasePath: "changelog/roqua/production",
+        /**
+         * Path to data on filesystem relative to site dir.
+         */
+        path: "./changelog/roqua/production",
+        blogSidebarCount: 50,
+
+        feedOptions: {
+          type: "all",
+          createFeedItems: async (params) => {
+            const { blogPosts, defaultCreateFeedItems, ...rest } = params;
+            return defaultCreateFeedItems({
+              // keep only the 10 most recent blog posts in the feed
+              blogPosts: blogPosts.filter((item, index) => index < 10),
+              ...rest,
+            });
+          },
+        },
       },
     ],
   ],
-
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'RoQua Docs',
+        title: "RoQua Docs",
         logo: {
-          alt: 'RoQua Logo',
-          src: 'img/logo.svg',
+          alt: "RoQua Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'User Manual',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "User Manual",
           },
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Technical documentation',
-            docsPluginId: 'technical'
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Technical documentation",
+            docsPluginId: "technical",
           },
           {
-            to: '/blog',
-            label: 'Releasenotes',
-            position: 'left'
+            to: "/blog",
+            label: "Blog",
+            position: "left",
           },
           {
-            to: '/status',
-            position: 'left',
-            label: 'Server Status',
+            to: "/changelog/roqua/production",
+            label: "Releasenotes",
+            position: "left",
+          },
+          {
+            to: "/status",
+            position: "left",
+            label: "Server Status",
           },
 
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
         ],
       },
